94684b77eef99fa1c57e74fa07c7e8ccde84c5e2
Norio Kobayashi <noriokob@gmail.com>
2016-11-27 20:14:15 +0900

    Update

---
+++ Assets/GaugeAnimation.cs
@@ -0,0 +1,32 @@
+﻿using DG.Tweening;
+using UnityEngine.UI;
+
+public class GaugeAnimation
+{
+    private Tweener _tweener;
+    private Image _gauge;
+
+    public float Amount { get { return _gauge.fillAmount; } }
+
+    public GaugeAnimation(Image gauge)
+    {
+        _gauge = gauge;
+        _gauge.fillAmount = 0;
+    }
+
+    public void Progress(float value)
+    {
+        if (_tweener != null) _tweener.Complete();
+
+        const float duration = 0.6f;
+
+        var now = _gauge.fillAmount;
+        var to = now + value;
+        _tweener = DOTween.To(
+                () => _gauge.fillAmount,
+                v => _gauge.fillAmount = v >= 1 ? v - (int) v : v,
+                to,
+                duration)
+            .SetEase(Ease.OutSine);
+    }
+}
---
+++ Assets/Main.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField]
+    private Image _gauge;
+
+    private GaugeAnimation _gaugeAnimation;
+
+	void Start()
+	{
+	    _gaugeAnimation = new GaugeAnimation(_gauge);
+    }
+
+    void OnGUI()
+    {
+        float p = 0;
+        if (GUI.Button(new Rect(20, 20, 100, 50), "+10"))
+        {
+            p = 0.1f;
+        }
+        else if (GUI.Button(new Rect(20, 80, 100, 50), "+30"))
+        {
+            p = 0.3f;
+        }
+        else if (GUI.Button(new Rect(20, 140, 100, 50), "+50"))
+        {
+            p = 0.5f;
+        }
+        else if (GUI.Button(new Rect(20, 200, 100, 50), "+100"))
+        {
+            p = 1.0f;
+        }
+        else if (GUI.Button(new Rect(20, 260, 100, 50), "+120"))
+        {
+            p = 1.2f;
+        }
+        else if (GUI.Button(new Rect(20, 320, 100, 50), "+150"))
+        {
+            p = 1.5f;
+        }
+
+        if (p > 0)
+        {
+            _gaugeAnimation.Progress(p);
+        }
+    }
+
+	void Update()
+	{
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        float p = 1.2f;
+	        _gaugeAnimation.Progress(p);
+            _gaugeAnimation.Progress(1.2f);
+	    }
+	}
+}
